d6c3229d9f1d4d2d538caa73647f31de1093fb68
Said León <122801569+SaidLeon04@users.noreply.github.com>
2023-10-11 19:27:26 -0600

    Update main.dart

--- 1_parcial/formulario/main.dart
+++ 1_parcial/formulario/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 import 'home_screen.dart';
 void main() {
   runApp(const MyApp());
@@ -10,7 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      //darkTheme: ThemeData.dark(),
+      // darkTheme: ThemeData.dark(),
       home:const HomePage() ,
     );
   }
@@ -31,7 +32,91 @@ class _HomePageState extends State<HomePage> {
   TextEditingController phoneController = TextEditingController();
   TextEditingController emailController = TextEditingController();
   TextEditingController ageController = TextEditingController();
- 
+  final GlobalKey<FormState> formKey = GlobalKey<FormState>();
+  final Future<SharedPreferences> _prefs = SharedPreferences.getInstance();
+  
+  bool nameExist = false;
+  bool lastExist = false;
+  bool plastlExist = false;
+  bool phoneExist = false;
+  bool emailExist = false;
+  bool ageExist = false;
+
+  void _submit(){
+    final isValid = formKey.currentState!.validate();
+    if (!isValid){
+      return;
+    }
+    formKey.currentState!.save();
+  }
+
+  saveData() async{
+    final SharedPreferences prefs = await _prefs;
+    prefs.setString("name", nameController.text.trim());
+    prefs.setString("last", lastController.text.trim());
+    prefs.setString("plast", plastlController.text.trim());
+    prefs.setString("phone", phoneController.text.trim());
+    prefs.setString("email", emailController.text.trim());
+    prefs.setString("age", ageController.text.trim());  
+    //print("Tu email es: " + emailController.text.trim());
+  }
+
+  Future<void> readData() async{
+    final SharedPreferences prefs = await _prefs;
+    final String? email = prefs.getString("email");
+    final String? name = prefs.getString("name");
+    final String? last = prefs.getString("last");
+    final String? plast = prefs.getString("plast");
+    final String? phone = prefs.getString("phone");
+    final String? age = prefs.getString("age");
+    if(email != null && name != null && last != null && plast != null && phone != null && age != null && context.mounted){
+      setState((){
+        nameExist = true;
+        lastExist = true;
+        plastlExist = true;
+        phoneExist = true;
+        emailExist = true;
+        ageExist = true;
+      });
+      if(nameExist && lastExist && plastlExist && phoneExist && emailExist && ageExist){
+        Navigator.push(
+          context,
+          MaterialPageRoute(
+            builder: (context) => LandingPage(
+              userEmail: email,
+              userName: name,
+              userLast: last,
+              userPlast: plast,
+              userPhone: phone,
+              userAge: age,
+
+              )),
+        );
+      }
+      //print(email);
+    }
+  }
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    readData();
+    /*if(emailExist){
+      Navigator.push(
+        context,
+        MaterialPageRoute(builder: (context) => LandingPage(
+          userEmail: emailController.text.trim(),
+          userName: nameController.text.trim(),
+          userLast: lastController.text.trim(),
+          userAge: ageController.text.trim(),
+          userPhone: phoneController.text.trim(),
+          userPlast: plastlController.text.trim()
+        )),
+      );
+    }*/
+
+  }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -41,25 +126,38 @@ class _HomePageState extends State<HomePage> {
           centerTitle: true,
           backgroundColor: Colors.black,
         ),
-        body: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: [
-            const SizedBox(
-              height: 5,
-            ),
-            const Center(child: Text("Login Page", style: TextStyle(
-                fontSize: 30,
-                fontWeight: FontWeight.bold,
-                color: Colors.black),
-            )),
-            const SizedBox(
-              height: 5,
-            ),
+        body: Form(
+          key: formKey,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              const SizedBox(
+                height: 5,
+              ),
+              const Center(child: Text("Login Page", style: TextStyle(
+                  fontSize: 30,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.black),
+              )),
+              const SizedBox(
+                height: 5,
+              ),
 
             Padding(
               padding: const EdgeInsets.all(8.0),
-              child: TextField(
+              child: TextFormField(
+                //valida pues
+                onFieldSubmitted: (value){
+                  print(value);
+                },
+                validator:(value){
+                  if(value!.isEmpty||!!RegExp(r"^[a-zA-Z0-9, a-zA-Z0-9.!#$%&'*+-/=?^_ {|}~]+@[a-zA-Z0-9]+\-[a-zA-Z]+")
+                  .hasMatch(value)){
+                    return "Escribe tu nombre";
+                  }
+                  return null;
+                },
                 controller: nameController,
                 decoration: const InputDecoration(
                     border: OutlineInputBorder(
@@ -74,35 +172,44 @@ class _HomePageState extends State<HomePage> {
             
             Padding(
               padding: const EdgeInsets.all(8.0),
-              child: TextField(
+              child: TextFormField(
+                //valida pues parte 2
+                onFieldSubmitted: (value){
+                  print(value);
+                },
+                validator:(value){
+                  if(value!.isEmpty||value.length<8){
+                    return "Escribe minimo 8 caracteres";
+                  }
+                  return null;
+                },
+                obscureText: true,
                 controller: lastController,
                 decoration: const InputDecoration(
                     border: OutlineInputBorder(
                       borderRadius: BorderRadius.all(Radius.circular(10)),
                     ),
-                    labelText: "Apellido Paterno"),
+                    labelText: "contraseña"),
               ),
             ),
             const SizedBox(
               height: 5,
             ),
+            
             Padding(
               padding: const EdgeInsets.all(8.0),
-              child: TextField(
-                controller: plastlController,
-                decoration: const InputDecoration(
-                    border: OutlineInputBorder(
-                      borderRadius: BorderRadius.all(Radius.circular(10)),
-                    ),
-                    labelText: "Apellido Materno"),
-              ),
-            ),
-            const SizedBox(
-              height: 5,
-            ),
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: TextField(
+              child: TextFormField(
+
+                onFieldSubmitted: (value){
+                  print(value);
+                },
+                validator: (value){
+                  if(value!.isEmpty||!!RegExp(r"^[a-zA-Z0-9, a-zA-Z0-9.!#$%&'*+-/=?^_ {|}~]+@[a-zA-Z0-9]+\-[a-zA-Z]+")
+                  .hasMatch(value)){
+                    return "Escribe tu nombre";
+                  }
+                  return null;
+                },
                 // obscureText: true, para ocultar contraseñas
                 controller: phoneController,
                 decoration: const InputDecoration(
@@ -115,9 +222,20 @@ class _HomePageState extends State<HomePage> {
             const SizedBox(
               height: 5,
             ),
+
             Padding(
               padding: const EdgeInsets.all(8.0),
-              child: TextField(
+              child: TextFormField(
+                onFieldSubmitted: (value){
+                  print(value);
+                },
+                validator: (value){
+                  if(value!.isEmpty||!!RegExp(r"^[a-zA-Z0-9, a-zA-Z0-9.!#$%&'*+-/=?^_ {|}~]+@[a-zA-Z0-9]+\-[a-zA-Z]+")
+                  .hasMatch(value)){
+                    return "Escribe tu nombre";
+                  }
+                  return null;
+                },
                 controller: emailController,
                 decoration: const InputDecoration(
                     border: OutlineInputBorder(
@@ -129,9 +247,20 @@ class _HomePageState extends State<HomePage> {
             const SizedBox(
               height: 5,
             ),
+
             Padding(
               padding: const EdgeInsets.all(8.0),
-              child: TextField(
+              child: TextFormField(
+                onFieldSubmitted: (value){
+                  print(value);
+                },
+                validator: (value){
+                  if(value!.isEmpty||!!RegExp(r"^[a-zA-Z0-9, a-zA-Z0-9.!#$%&'*+-/=?^_ {|}~]+@[a-zA-Z0-9]+\-[a-zA-Z]+")
+                  .hasMatch(value)){
+                    return "Escribe tu nombre";
+                  }
+                  return null;
+                },
                 controller: ageController,
                 decoration: const InputDecoration(
                     border: OutlineInputBorder(
@@ -143,19 +272,36 @@ class _HomePageState extends State<HomePage> {
             const SizedBox(
               height: 5,
             ),
+            
             Padding(
               padding: const EdgeInsets.only(left: 10.0, right: 10.0),
               child: ElevatedButton(
                   onPressed: () {
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(builder: (context) =>
-                          LandingPage(userName: nameController.text.trim(), userLast: lastController.text.trim(),
-                          userPlast: plastlController.text.trim(), userPhone: phoneController.text.trim(),
-                          userEmail: emailController.text.trim(), userAge: ageController.text.trim(),)
+                    _submit();
+                    saveData();
+                    final isValid = formKey.currentState!.validate();
+                    if(isValid){
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(builder: (context) =>
+                          LandingPage(
+                          userName: nameController.text.trim(), 
+                          userLast: lastController.text.trim(),
+                          userPlast: plastlController.text.trim(), 
+                          userPhone: phoneController.text.trim(),
+                          userEmail: emailController.text.trim(),
+                          userAge: ageController.text.trim(),
+                          )
                           ),
-                          
-                    );
+                      ).then((res){
+                        nameController.clear();
+                        lastController.clear();
+                        plastlController.clear();
+                        phoneController.clear();
+                        emailController.clear();
+                        ageController.clear();
+                      });
+                    }
                   },
                   child: const Text(
                     "Login",
@@ -163,6 +309,7 @@ class _HomePageState extends State<HomePage> {
                   )),
             )
           ],
+        ),
         ));
   }
 }
